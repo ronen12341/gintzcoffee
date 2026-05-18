7ce8fcc5a8e533687368f5d0be36d04b86841ecc
Ronen <ronen12341@gmail.com>
2026-05-18 18:08:03 +0300

    Fix coffee bag image crop

--- src/app/beans/page.tsx
+++ src/app/beans/page.tsx
@@ -57,7 +57,7 @@ export default function BeansPage() {
                   <img
                     src={bean.image}
                     alt={bean.name}
-                    className="w-full aspect-[3/4] object-contain bg-white p-4"
+                    className="w-full aspect-[3/4] object-cover bg-white"
                   />
                 ) : (
                   <ImagePlaceholder label="הוסף תמונה" width={300} height={400} />
